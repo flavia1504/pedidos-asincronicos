6ff6dd34b21d3fc94e9149837105d45a3a8bfd33
flavia1504 <milhouse.mb@gmail.com>
2024-04-23 22:35:32 -0300

    pedidos asincronicos

---
+++ src/routes/moviesRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/moviesController');
+//------------- Clase 47 ---------------
+router.get('/movies/list', moviesController.listNew);
+router.get("/movies/createNew", moviesController.createNew)
+router.get('/movies/favorites', moviesController.favorites);
+
+//---------------------------------------
+
+router.get('/movies', moviesController.list);
+router.get('/movies/new', moviesController.new);
+router.get('/movies/recommended', moviesController.recomended);
+router.get('/movies/detail/:id', moviesController.detail);
+//Rutas exigidas para la creación del CRUD
+router.get('/movies/add', moviesController.add);
+router.post('/movies/create', moviesController.create);
+router.get('/movies/edit/:id', moviesController.edit);
+router.put('/movies/update/:id', moviesController.update);
+router.get('/movies/delete/:id', moviesController.delete);
+router.delete('/movies/delete/:id', moviesController.destroy);
+
+module.exports = router;
